217a4862c65c5ca1c21511300cb7a74040d45595
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2025-03-11 22:59:39 -0600

    get args

--- Module06_Challenge/src/main.c
+++ Module06_Challenge/src/main.c
@@ -1,7 +1,22 @@
 #include "paged_mem.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello, world!\n");
-  return 0;
+void print_usage(const char *progname) {
+  fprintf(stderr, "Usage: %s <memory_file>\n", progname);
 }
+
+int main(int argc, char *argv[]) {
+  // Check if the memory file argument was provided.
+  if (argc < 2) {
+    print_usage(argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  // If the argument is provided, store it for later use.
+  char *memory_file = argv[1];
+  printf("Memory file provided: %s\n", memory_file);
+
+  return EXIT_SUCCESS;
+}
+
